2a3deca4c8a996286c41568941a08e57f9ebbf11
Kurt Endless <kendis_it@hotmail.com>
2020-03-28 19:48:40 +0200

    add: init client part

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../root.css";
+
+const App = () => {
+    return(
+        <div>Hello world</div>
+    );
+}
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import reducer from "./reducers";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
