32bbf42005a80a05f8e780fc0678a355d68310a5
Kurt Endless <kendis_it@hotmail.com>
2020-03-28 13:40:44 +0200

    update: configuration prod

--- api/sequelize/config/config.js
+++ api/sequelize/config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+    "dialect": "postgres",
+    "host": process.env.DB_HOST,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "port": process.env.DB_PORT
   }
 }
