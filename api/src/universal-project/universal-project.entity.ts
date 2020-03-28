bf63fde1fbae2299530c26c67567b60c3313c3fd
Kurt Endless <kendis_it@hotmail.com>
2020-03-28 18:22:06 +0200

    add: universal-project

---
+++ api/src/universal-project/universal-project.entity.ts
@@ -0,0 +1,42 @@
+import {
+	Model,
+	Column,
+	DataType,
+	Table,
+	CreatedAt,
+    UpdatedAt,
+} from "sequelize-typescript";
+
+import IIndex from "../requirement/interfaces/index.interface";
+
+@Table({ tableName: "projects" })
+export class Project extends Model<Project> {
+	@Column({
+		type: DataType.BIGINT,
+		primaryKey: true,
+	})
+	public id: number;
+
+	@Column({
+		type: DataType.STRING,
+	})
+	public name: string;
+
+	@Column({
+		type: DataType.JSONB,
+		allowNull: true,
+	})
+	public profile: IIndex[];
+
+	@CreatedAt
+	@Column({
+		type: DataType.DATE,
+	})
+    public createdAt: Date;
+    
+    @UpdatedAt
+    @Column({
+        type: DataType.DATE
+    })
+    public updatedAt: Date;
+}
