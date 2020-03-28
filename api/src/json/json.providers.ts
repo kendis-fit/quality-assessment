3745db04481f5437cd1867eee2f649d93b43aa9e
Kurt Endless <kendis_it@hotmail.com>
2020-03-28 13:32:56 +0200

    update: universal provider for json mo

--- api/src/json/json.providers.ts
+++ api/src/json/json.providers.ts
@@ -1,10 +1,12 @@
 import { profile } from "./profile.json";
 import { uxProfile } from "./ux-profile.json.js";
 import { baseProfile } from "./base-profile.json.js";
+import { Profile } from "./profile.enum.js";
 
 export const PROFILE = "PROFILE";
 export const UX_PROFILE = "UX_PROFILE";
 export const BASE_PROFILE = "BASE_PROFILE";
+export const TYPE_PROFILE = "TYPE_PROFILE";
 
 export const jsonProviders = [
 	{
@@ -19,4 +21,15 @@ export const jsonProviders = [
 		provide: BASE_PROFILE,
 		useValue: baseProfile,
 	},
+	{
+		provide: TYPE_PROFILE,
+		useValue: (prof: Profile) => {
+			switch (prof) {
+				case PROFILE: return profile; 
+				case UX_PROFILE: return uxProfile;
+				case BASE_PROFILE: return baseProfile;
+				default: return [];
+			}
+		}
+	}
 ];
---
+++ api/src/json/profile.enum.ts
@@ -0,0 +1,5 @@
+export enum Profile {
+    PROFILE = "PROFILE",
+    UX_PROFILE = "UX_PROFILE",
+    BASE_PROFILE = "BASE_PROFILE",
+}
