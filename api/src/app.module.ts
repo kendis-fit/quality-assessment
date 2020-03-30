789dcc1e9d3c0f51a1699ea0e693c7ed2b249cba
Kurt Endless <kendis_it@hotmail.com>
2020-03-30 23:35:23 +0300

    add: user model

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from "@nestjs/common";
 import { ProjectModule } from "./project/project.module";
 import { RequirementModule } from "./requirement/requirement.module";
 import { UniversalProjectModule } from "./universal-project/universal-project.module";
+import { UserModule } from './user/user.module';
 
 @Module({
-	imports: [RequirementModule, ProjectModule, UniversalProjectModule],
+	imports: [RequirementModule, ProjectModule, UniversalProjectModule, UserModule],
 })
 export class AppModule {}
---
+++ api/src/user/user.entity.ts
@@ -0,0 +1,31 @@
+import { Model, Table, Column, DataType, HasMany } from "sequelize-typescript";
+
+import { Requirement } from "src/requirement/requirement.entity";
+import { Project } from "src/universal-project/universal-project.entity";
+
+@Table({ tableName: "users" })
+export class User extends Model<User> {
+    @Column({
+        type: DataType.UUID,
+        primaryKey: true,
+        defaultValue: DataType.UUIDV4
+    })
+    public id: string;
+
+    @Column({
+        type: DataType.STRING
+    })
+    public name: string;
+
+    @Column({
+        type: DataType.STRING
+    })
+    public password: string;
+
+    @HasMany(() => Requirement)
+    public requirements: Requirement;
+
+    @HasMany(() => Project)
+    public projects: Project;
+
+}
