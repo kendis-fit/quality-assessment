220437869eb5b68657104b64e7510509a5c237e1
Kurt Endless <kendis_it@hotmail.com>
2020-03-31 18:12:01 +0300

    add: getUserByEmail

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { ConfigModule } from 'src/config/config.module';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-	imports: [DatabaseModule],
+	imports: [DatabaseModule, ConfigModule],
 	controllers: [UserController],
 	providers: [UserService]
 })
--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, HttpException, HttpStatus } from '@nestjs/common';
+
+import { User } from './user.entity';
+import { USER_REPOSITORY } from './user.providers';
+import { ConfigService } from 'src/config/config.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private readonly jwtSecretKey: string;
+
+    public constructor(@Inject(USER_REPOSITORY)private users: typeof User, private configService: ConfigService) {
+        this.jwtSecretKey = configService.JwtSecretKey;
+    }
+
+    public login() {
+
+    }
+
+    public registration() {
+
+    }
+
+    public async getUserByEmail(email: string): Promise<User> {
+        const user = await this.users.findOne({
+            where: {
+                email: email
+            }
+        });
+        if (!user) {
+            throw new HttpException("", HttpStatus.NOT_FOUND);
+        }
+        return user;
+    }
+}
