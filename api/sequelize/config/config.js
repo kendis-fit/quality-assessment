1904c64c18f1c0d14ab9092b40e31f1379018bcc
Kurt Endless <kendis_it@hotmail.com>
2020-03-31 20:16:20 +0300

    add: model user and change name of dev db

--- api/sequelize/config/config.js
+++ api/sequelize/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "host": "localhost",
     "username": "postgres",
     "password": "12345",
-    "database": "SoftwareRequirements",
+    "database": "QualityDB",
     "port": "5432"
   },
   "test": {
--- api/src/database/database.providers.ts
+++ api/src/database/database.providers.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 
+import { User } from "src/user/user.entity";
 import { ConfigService } from "src/config/config.service";
 import { Requirement } from "src/requirement/requirement.entity";
 import { Project } from "src/universal-project/universal-project.entity";
@@ -11,7 +12,7 @@ export const databaseProviders = [
 		provide: SEQUELIZE,
 		useFactory: async (config: ConfigService) => {
 			const sequelize = new Sequelize(config.Database);
-			sequelize.addModels([Requirement, Project]);
+			sequelize.addModels([Requirement, Project, User]);
 			await sequelize.sync();
 			return sequelize;
 		},
