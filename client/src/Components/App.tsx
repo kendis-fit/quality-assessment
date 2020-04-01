59243a409ff082d58d1f1e157a36ef8e77ea6add
Kurt Endless <kendis_it@hotmail.com>
2020-04-01 21:23:32 +0300

    add: Login component

--- client/src/Components/App.tsx
+++ client/src/Components/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "../root.css";
-import Profile from "./Profile/Profile";
-import Registration from "./Registration/Registration";
+import Login from "./Login";
+import Profile from "./Profile";
+import Registration from "./Registration";
 
 const App = () => {
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact path="/registration" component={Registration} />
-                <Route exact path={["/login", "/"]} />
+                <Route exact path={["/login", "/"]} component={Login} />
                 <Route exact path="/universal-projects/:id" render={props => <Profile {...props} isRequirement={false} />} />
                 <Route exact path="/requirements/:id" render={props => <Profile {...props} isRequirement={true} />} />
             </Switch>
---
+++ client/src/Components/Login/Login.tsx
@@ -0,0 +1,115 @@
+import * as yup from "yup";
+import { Formik, Form } from "formik";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import { Grid, Link as LinkComponent, makeStyles, TextField, Button, Typography, FormControl, InputLabel, InputAdornment, IconButton, FormHelperText, OutlinedInput } from "@material-ui/core";
+
+import background from "../../Images/background-registration.jpg";
+
+const initialValues = {
+    email: "",
+    password: ""
+}
+
+const schema = yup.object().shape({
+    email: yup.string()
+        .required("Email is required")
+        .email("Email is invalid"),
+    password: yup.string()
+        .required("Password is required")
+});
+
+const useStyles = makeStyles({
+    title: {
+        fontSize: "24px",
+        marginTop: "75px",
+        marginBottom: "20px"
+    },
+    form: {
+        background: `url(${background}) no-repeat`,
+        backgroundSize: "cover",
+        width: "100",
+        height: "100vh"
+    },
+    formContent: {
+        background: "rgba(255, 255, 255, 1)",
+        width: "500px",
+        height: "100vh"
+    },
+    textFields: {
+        width: "400px"
+    },
+    link: {
+        marginTop: "20px"
+    }
+});
+
+const Login = () => {
+    
+    const classes = useStyles(); 
+    const [showPassword, setShowPassword] = useState(false);
+
+    return(
+        <Formik 
+            initialValues={initialValues}
+            validationSchema={schema}
+            validateOnBlur={false}
+            validateOnChange={false}
+            onSubmit={values => console.log(values)}
+            >
+            {
+                ({ values, handleChange, errors }) => (
+                    <Form className={classes.form}>
+                        <Grid className={classes.formContent} alignContent="center" container direction="column">
+                            <Typography className={classes.title} align="center">Product Quality Assessment</Typography>
+                            <TextField 
+                                name="email"
+                                label="Email"
+                                value={values.email}
+                                onChange={handleChange}
+                                error={!!errors.email}
+                                helperText={errors.email || " "}
+                                variant="outlined"
+                                color="primary"
+                                size="small"
+                                className={classes.textFields}
+                                />
+                            <FormControl error={!!errors.password} variant="outlined" size="small">
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <OutlinedInput
+                                    name="password"
+                                    id="password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    color="primary"
+                                    labelWidth={80}
+                                    endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                            >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                    }
+                                />
+                                <FormHelperText>{errors.password || " "}</FormHelperText>
+                            </FormControl>
+                            <Button type="submit" variant="contained" color="primary">
+                                <Typography>Sign in</Typography>
+                            </Button>
+                            <LinkComponent className={classes.link} align="center">
+                                <Link to="/registration">Create an account</Link>
+                            </LinkComponent>
+                        </Grid>
+                    </Form>
+                )
+            }
+        </Formik>
+    )
+}
+
+export default Login;
