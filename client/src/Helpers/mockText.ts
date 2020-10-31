e296ed3c87c1146996af79d55416badcc7025279
Kurt Endless <kendis_it@hotmail.com>
2020-10-31 16:38:48 +0200

    added new replacable features

--- client/src/Helpers/mockText.ts
+++ client/src/Helpers/mockText.ts
@@ -1,5 +1,5 @@
-import { rand } from "./random";
-import { replaceFrom } from "./strings";
+import { rand, randArrayOfValues } from "./random";
+import { replaceFrom, replaceLastWordFrom } from "./strings";
 
 interface IMockOptions {
     mathOperations: number,
@@ -21,6 +21,7 @@ const mockString = (
     maxMockingRate: number,
     regExp: RegExp,
     replacedSelection: string[],
+    replaceFunc: (string: string, index: number, regExp: RegExp, replacedString: string) => string,
     codeNumber?: number,
     getMockedStringCallback?: GetMockedStringCallback
 ): IMockStringResult => {
@@ -36,8 +37,8 @@ const mockString = (
     }
     do {
         const randIndex = rand(0, replacedSelection.length);
-        mockedString = replaceFrom(string, counterOfMockedSymbols, regExp, replacedSelection[randIndex]);
-        modifiedMockedString = replaceFrom(modifiedMockedString, counterOfMockedSymbols, regExp, replacedSelection[randIndex]);
+        mockedString = replaceFunc(string, counterOfMockedSymbols, regExp, replacedSelection[randIndex]);
+        modifiedMockedString = replaceFunc(modifiedMockedString, counterOfMockedSymbols, regExp, replacedSelection[randIndex]);
         if (mockedString !== string) {
             ++counterOfMockedSymbols;
             getMockedStringCallback?.(codeNumber as number, mockedString, string);
@@ -55,27 +56,37 @@ const mockString = (
 const mockText = (text: string, options: IMockOptions, getMockedStringCallback?: GetMockedStringCallback): string => {
     const logicalOperations = ["!", "||", "&&"];
     const mathOperations = ["+", "/", "*", "-"];
+    const valuesConstants = randArrayOfValues(50).map((value) => `${value};`);
+    const nameVariables = randArrayOfValues(50, true);
 
     let mathOperationCount = 0;
     let logicalOperationCount = 0;
     let valueConstantsCount = 0;
+    let nameVariablesCount = 0;
 
     const splitedText = text.split('\n');
     const mockedText = splitedText.map((string, codeNumber) => {
         let mockedString = string;
         if (mathOperationCount < options.mathOperations) {
-            const result = mockString(string, mockedString, options.mathOperations - mathOperationCount, /[+-/*]/g, mathOperations, codeNumber + 1, getMockedStringCallback);
+            const result = mockString(string, mockedString, options.mathOperations - mathOperationCount, /[+-/*]/g, mathOperations, replaceFrom, codeNumber + 1, getMockedStringCallback);
             mockedString = result.modifiedMockedString;
             mathOperationCount += result.count;
         }
         if (logicalOperationCount < options.logicalOperations) {
-            const result = mockString(string, mockedString, options.logicalOperations - logicalOperationCount, /(!|(\s&{2}\s)|(\s\|{2}\s))/g, logicalOperations, codeNumber + 1, getMockedStringCallback);
+            const result = mockString(string, mockedString, options.logicalOperations - logicalOperationCount, /(!|(\s&{2}\s)|(\s\|{2}\s))/g, logicalOperations, replaceFrom, codeNumber + 1, getMockedStringCallback);
             mockedString = result.modifiedMockedString;
             logicalOperationCount += result.count;
         }
-        // if (valueConstantsCount !== options.valueConstants) {
-        //     const result = mockString(string, mockedString, options.valueConstants, /const\s(unsigned|signed|short)?\s+(int|string|bool|char|long|double|float)\s+\w+\s*=\s*(("|').*("|')|false|true|-?\d*\.?\d*);/g, ['test'], codeNumber + 1, getMockedStringCallback);
-        // }
+        if (valueConstantsCount < options.valueConstants) {
+            const result = mockString(string, mockedString, options.valueConstants - valueConstantsCount, /const\s((unsigned|signed|short)\s+)?(int|string|bool|char|long|double|float)\s+\w+\s*=\s*(("|').*("|')|false|true|-?\d*\.?\d*);/g, valuesConstants, replaceLastWordFrom, codeNumber + 1, getMockedStringCallback);
+            mockedString = result.modifiedMockedString;
+            valueConstantsCount += result.count;
+        }
+        if (nameVariablesCount < options.nameVariables) {
+            const result = mockString(string, mockedString, options.nameVariables - nameVariablesCount, /(int|string|bool|char|long|double|float)\s+[^\d][a-z\d]+(?=[\s=,])/g, nameVariables, replaceLastWordFrom, codeNumber + 1, getMockedStringCallback);
+            mockedString = result.modifiedMockedString;
+            nameVariablesCount += result.count;
+        }
         return mockedString;
     });
 
--- client/src/Helpers/random.ts
+++ client/src/Helpers/random.ts
@@ -1,3 +1,5 @@
+import faker from "faker";
+
 export const generateRandom = (): number => Number(String(Math.random()).slice(2));
 
 export const rand = (begin: number = 0, end: number = 1) => {
@@ -9,6 +11,22 @@ export const randArray = (length: number) => {
     const array: number[] = [];
     for (let i = 1; i < length; ++i) {
         array.push(rand());
+    }
+    return array;
+};
+
+export const randArrayOfValues = (length: number, onlyWords = false) => {
+    const array: string[] = [];
+    for (let i = 1; i < length; ++i) {
+        if (onlyWords) {
+            array.push(faker.random.word());
+        } else {
+            if (i % 2 === 0) {
+                array.push(faker.random.word());
+            } else {
+                array.push(faker.random.number().toString());
+            }
+        }
     }
     return array;
 };
